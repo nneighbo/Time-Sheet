7993b2e3171f85a7936c3c1e4686adfdf6bce7bb
LBergen <lars.derek.bergen@gmail.com>
2018-07-12 12:44:12 -0700

    jj

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var employeeName = ""; 
+var employeeRole = "";
+var employeeRate = 0;
+var employeeStart = 0;
+
+$("#new-employee-submit").on("click", function(event) {
+event.preventDefault();
+$("#employee-name").text(snapshot.val();
+console.log(this);
+$("#employee-role")
+$("#employee-start-date")
+$("#employee-monthly-rate")
+
+
+
+});
